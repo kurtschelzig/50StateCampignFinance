d8034e8d0cab5b43888ef9c7bd2e2c2fbee40b7e
Kurt1111 <kurtschelzig@gmail.com>
2026-02-06 01:54:35 -0600

    Update 50StateExport.R
    Added Connecticut, Georgia, Florida, Illinois, Kansas, Kentucky, Louisiana, and Maine. I also Added a screenshot function and a "Check if element exists" function.  The CompileAndPort function added a TSV function to process Florida data. 
    
    Several states block the docker instance because it lacks screen dimensions, I am working on spoofing screen dimensions on the open port to resolve this issue

--- 50StateExport.R
+++ 50StateExport.R
@@ -3,19 +3,35 @@
     install.packages(RSelenium)
   }
   if (require("readxl") == FALSE ){
-    install.packages()
+    install.packages(readxl)
+  }
+  if (require("readxl") == FALSE ){
+    install.packages(readr)
   }
   library(RSelenium)
   library(readxl)
+  library(readr)
 ################################################################################
   remDr <- remoteDriver( # Connects to Selenium Instance
               remoteServerAddr = "localhost",
               port = 4445L,
-              browserName = "firefox",
-              extraCapabilities = FireFox
+              browserName = "firefox"
               )
+  
+
   remDr$open()
   
+ss <- function(){
+  remDr$screenshot(display = TRUE)
+}
+element_exists <- function(remDr, using, value) {
+  tryCatch({
+    elem <- remDr$findElement(using = using, value = value)
+    return(!is.null(elem))
+  }, error = function(e) {
+    return(FALSE)  # Element not found
+  })
+} 
 ################################################################################     
 #Convert Docker Tmp folder .part files
   ClearTmp <- function() {
@@ -36,15 +52,159 @@
                 include.dirs = FALSE,
                 full.names = FALSE     
                 )
+    if(State == "Florida"){
+      for(i in 1:length(files)){
+        file.rename(from = paste("./Processing","/report",i,".csv",sep=""), to = paste("./Processing","/report",i,".tsv",sep=""))
+      }
+      CompiledReport <- read_tsv("./Processing/report1.tsv")
+      file.remove("./Processing/report1.tsv")
+      if(length(files)>2){
+        for(i in 2:length(files)){
+          CompiledReport <-  rbind(CompiledReport, read_tsv(paste("./Processing/report",i,".tsv",sep="")))
+          file.remove(paste("./Processing/report",i,".tsv", sep = ""))
+        }
+      }
+    }
+    else{
     CompiledReport <- read.csv("./Processing/report1.csv")
     file.remove("./Processing/report1.csv")
+    if(length(files)>2){
     for(i in 2:length(files)){
       CompiledReport <-  rbind(CompiledReport,read.csv(paste("./Processing/report",i,".csv", sep = "")))
       file.remove(paste("./Processing/report",i,".csv", sep = ""))
     }
-    write.csv(CompiledReport,paste("./",State,"/",State,Year,".csv", sep = ""))
+    }
+    }
+    write.csv(CompiledReport,paste("./",State,"/",State,Year,".csv", sep = ""), na="")
   }
-################################################################################  
+################################################################################
+  Connecticut<- function(Year){
+    for(i in Year){
+      link <- paste("https://seec.ct.gov/ecrisreporting/Data/eCrisDownloads/exportdatafiles/Receipts",i,"ElectionYearCandidateExploratoryCommittees.csv",sep="")
+      remDr$navigate(link)
+      ClearTmp()
+      CompileAndPort("Connecticut",i)
+    }
+  }
+  Georgia <- function(Year){
+    for(i in Year){
+      link <- paste("https://media.ethics.ga.gov/search/Campaign/Campaign_ByContributionsearchresults.aspx?Contributor=&Zip=&City=&ContTypeID=0&PAC=&Employer=&Occupation=&From=1/1/",i,"&To=12/31/",i,"&Cash=&InK=&Filer=&Candidate=&Committee=",sep="")
+      remDr$navigate(link)
+      remDr$findElement("css","#ctl00_ContentPlaceHolder1_Export")$clickElement()
+      ClearTmp()
+      CompileAndPort("Georgia",i)
+    }
+  }
+  Florida <- function(Year){
+    GetPage <- function(GetYear, Limit, Export){
+      remDr$navigate("https://dos.elections.myflorida.com/campaign-finance/contributions/#criteria")
+      Sys.sleep(1)
+      remDr$findElement("css","#rightContent > form > select:nth-child(5)")$sendKeysToElement(list("A", "\uE007"))
+      remDr$findElement("css","#rightContent > form > div.dateRange > div > input:nth-child(1)")$sendKeysToElement(list(paste("1/1/",GetYear, sep = "")))
+      remDr$findElement("css","#rightContent > form > div.dateRange > div > input:nth-child(2)")$sendKeysToElement(list(paste("12/31/",GetYear, sep = "")))
+      remDr$findElement("css","#rightContent > form > div:nth-child(61) > input")$clearElement()
+      remDr$findElement("css","#rightContent > form > div:nth-child(61) > input")$sendKeysToElement(list(as.character(Limit)))
+      if(Export == TRUE){
+        remDr$findElement("css","#rightContent > form > ul:nth-child(67) > li:nth-child(2) > input[type=radio]")$clickElement()
+      }
+      remDr$findElement("css","#rightContent > form > div:nth-child(68) > input[type=submit]:nth-child(2)")$clickElement()
+    }
+    GetLength <- function(SampleYear){
+      UpperBound <- 99999
+      LowerBound <- 0
+      SampleLimit <- 50000
+      while((UpperBound-LowerBound) >1){
+        GetPage(SampleYear,SampleLimit, FALSE)
+        Sys.sleep(1)
+        if(element_exists(remDr,"css","body > pre > a > address")){
+          UpperBound <- SampleLimit
+          SampleLimit <- round((UpperBound+LowerBound)/2,0)
+        }
+        else{
+          LowerBound <- SampleLimit
+          SampleLimit <- round((UpperBound+LowerBound)/2,0)
+        }
+        print(paste(UpperBound,LowerBound,SampleLimit))
+      }
+    return(LowerBound)
+    }
+    for(i in Year){
+      Records <- GetLength(i)
+      print(Records)
+      GetPage(i,Records,TRUE)
+      ClearTmp()
+      CompileAndPort("Florida",i)
+    }
+  }
+  Illinois <- function(Year){
+    for(i in Year){
+      for(j in 1:12){
+        remDr$navigate("https://www.elections.il.gov/CampaignDisclosure/ContributionSearchByAllContributions.aspx")
+        Sys.sleep(2)
+        if(j == 1){
+          remDr$findElement("css"," #ContentPlaceHolder1_txtRcvDate")$sendKeysToElement(list(paste("1/1/",i, sep = "")))
+          remDr$findElement("css"," #ContentPlaceHolder1_txtRcvDateThru")$sendKeysToElement(list(paste(j+1,"/1/",i, sep = ""),key = "enter"))
+        } else if(j == 12){
+          remDr$findElement("css"," #ContentPlaceHolder1_txtRcvDate")$sendKeysToElement(list(paste(j,"/2/",i, sep = "")))
+          remDr$findElement("css"," #ContentPlaceHolder1_txtRcvDateThru")$sendKeysToElement(list(paste(j,"/31/",i, sep = ""),key = "enter"))
+        } else{
+          remDr$findElement("css"," #ContentPlaceHolder1_txtRcvDate")$sendKeysToElement(list(paste(j,"/2/",i, sep = "")))
+          remDr$findElement("css"," #ContentPlaceHolder1_txtRcvDateThru")$sendKeysToElement(list(paste(j+1,"/1/",i, sep = ""),key = "enter"))
+        }
+        Sys.sleep(2)
+        remDr$findElement("css","#ContentPlaceHolder1_lnkDownloadList")$clickElement()
+        Sys.sleep(2)
+        remDr$findElement("css","#ContentPlaceHolder1_btnCSV")$clickElement()
+        Sys.sleep(2)
+      }
+     ClearTmp()
+     CompileAndPort("Illinois",i)
+    }
+  }
+  Kansas <- function(Year){
+    for(i in Year){
+      remDr$navigate("https://sos.ks.gov/elections/cfr_viewer/cfr_examiner_contribution.aspx")
+      remDr$findElement("css","#txtStartDate")$sendKeysToElement(list(paste("1/1/",Year),key="enter"))
+      remDr$findElement("css","#txtEndDate")$sendKeysToElement(list(paste("12/31/",Year),key="enter"))
+      remDr$findElement("css","#btnSubmit.smallbutton")$clickElement()
+      remDr$findElement("css","#btnExport")$clickElement()
+      ClearTmp()
+      CompileAndPort("Kansas",i)
+    }
+  }
+  Kentucky <- function(Year){
+    for(i in Year){
+    link <- paste("https://secure.kentucky.gov/kref/publicsearch/AllContributors?PageIndex=0&FirstName=&LastName=&FromOrganizationName=&ElectionDate=01%2F01%2F0001&City=&State=&Zip=&Employer=&Occupation=&OtherOccupation=&MinAmount=&MaxAmount=&MinimalDate=",i,"-01-01&MaximalDate=",i,"-12-31&ContributionMode=&ContributionSearchType=All&PageSize=10&PageIndex=0&ReportId=",sep = "")
+    remDr$navigate(link)
+    Sys.sleep(2)
+    remDr$findElement("css","#main-wrapper > div > div > div:nth-child(2) > a > i")$clickElement()
+    ClearTmp()
+    CompileAndPort("Kentucky",i)
+    }
+  }
+  Louisiana <- function(Year){
+    for(i in Year){
+      remDr$navigate("https://www.ethics.la.gov/CampaignFinanceSearch/SearchEfilingContributors.aspx")
+      remDr$findElement("css","#ctl00_ContentPlaceHolder1_DateFromRadDateInput")$sendKeysToElement(list(paste("1/1/",i)))
+      remDr$findElement("css","#ctl00_ContentPlaceHolder1_DateToRadDateInput")$sendKeysToElement(list(paste("12/31/",i)))
+      remDr$findElement("css","#ctl00_ContentPlaceHolder1_PerformSearchLinkButton")$clickElement()
+      Sys.sleep(2)
+      remDr$findElement("css","#ctl00_ContentPlaceHolder1_ExportToCSVLinkButton")$clickElement()
+      ClearTmp()
+      CompileAndPort("Louisiana",i)
+    }
+  }
+  
+  Maine <- function(Year){
+    remDr$navigate("https://mainecampaignfinance.com/#/dataDownload")
+    remDr$findElement("css","#girdrow > tbody > tr > td.page-selector > div > select")$sendKeysToElement(list("\uE015","\uE015","\uE007"))
+    for(i in Year){
+      remDr$findElement("css",paste("#page-content-wrapper > div > div.table-responsive > table > tbody > tr:nth-child(",((2*(18-(i-2008)))+1),") > td:nth-child(3) > a",sep=""))$clickElement()
+      Sys.sleep(2)
+      ClearTmp()
+      CompileAndPort("Maine",i)
+    }
+  }
   Tennessee <-function(Year){
     # Search Year with Settings
     SearchYear <- function(SampleYear){
@@ -108,8 +268,7 @@
     CompileAndPort("Tennessee",Year[i])
   }
 }
-  
-Tennessee(c(2024:2025))  
-  
+
+ 
 remDr$close()
   
